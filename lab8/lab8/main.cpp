3f50ec0b5eae106bffa5c1f1fc668594e7c3bfd3
coteacarla <161612371+coteacarla@users.noreply.github.com>
2024-05-15 17:31:58 +0300

    main.cpp

--- lab8/lab8/main.cpp
+++ lab8/lab8/main.cpp
@@ -14,9 +14,9 @@ class Compare {
 public:
     bool operator() (pair<string, int> p1, pair<string, int> p2) {
         if (p1.second < p2.second)
-            return true;
-        if (p1.second > p2.second)
             return false;
+        if (p1.second > p2.second)
+            return true;
         if (p1.first < p2.first)
             return true;
         return false;
